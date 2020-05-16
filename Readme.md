a121c292397a69c8c07bef356d2db48fd80cd44a
Nathan Rajlich <nathan@tootallnate.net>
2020-05-16 03:01:03 -0700

    Add `Readme.md`

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# color
+
+Print ANSI color escape sequences in shell scripts.
+
+## Example
+
+```bash
+#!/usr/bin/env import
+import "color"
+
+echo "`blue some blue text,` `red and now red,` `and green suddenly green!`"
+```
--- color.sh
+++ color.sh
@@ -1,7 +1,26 @@
-BOLD="$(tput bold 2>/dev/null ||:)"
-GREY="$(tput setaf 0 2>/dev/null ||:)"
-RED="$(tput setaf 1 2>/dev/null ||:)"
-GREEN="$(tput setaf 2 2>/dev/null ||:)"
-MAGENTA="$(tput setaf 5 2>/dev/null ||:)"
-CYAN="$(tput setaf 6 2>/dev/null ||:)"
-NO_COLOR="$(tput sgr0 2>/dev/null ||:)"
+COLOR_BOLD="$(tput bold 2>/dev/null ||:)"
+COLOR_BLACK="$(tput setaf 0 2>/dev/null ||:)"
+COLOR_RED="$(tput setaf 1 2>/dev/null ||:)"
+COLOR_GREEN="$(tput setaf 2 2>/dev/null ||:)"
+COLOR_YELLOW="$(tput setaf 3 2>/dev/null ||:)"
+COLOR_BLUE="$(tput setaf 4 2>/dev/null ||:)"
+COLOR_MAGENTA="$(tput setaf 5 2>/dev/null ||:)"
+COLOR_CYAN="$(tput setaf 6 2>/dev/null ||:)"
+COLOR_WHITE="$(tput setaf 7 2>/dev/null ||:)"
+COLOR_RESET="$(tput sgr0 2>/dev/null ||:)"
+
+red() {
+  printf "%s" "${COLOR_RED}$*${COLOR_RESET}"$'\n'
+}
+
+green() {
+  printf "%s" "${COLOR_GREEN}$*${COLOR_RESET}"$'\n'
+}
+
+blue() {
+  printf "%s" "${COLOR_BLUE}$*${COLOR_RESET}"$'\n'
+}
+
+cyan() {
+  printf "%s" "${COLOR_CYAN}$*${COLOR_RESET}"$'\n'
+}
